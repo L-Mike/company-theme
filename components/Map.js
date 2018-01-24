3ef4bdf1bc16c9005b69835b564a0981418e5aff
Miloš Lalić <miloslalic9@gmail.com>
2018-01-24 21:14:58 +0100

    Initial commit

---
+++ components/Map.js
@@ -0,0 +1,20 @@
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const Map = withScriptjs(withGoogleMap((props) => (
+    <GoogleMap
+        defaultZoom={12}
+        defaultCenter={{
+            lat: 41.878114,
+            lng: -87.629798,        
+        }}
+        defaultOptions={{
+            scrollwheel: false,
+            draggable: false
+        }}
+        >
+        {props.isMarkerShown && <Marker position={{lat: 41.878114, lng: -87.629798}} />}
+    </GoogleMap>        
+    ))
+);
+
+export default Map;
