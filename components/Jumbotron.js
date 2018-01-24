3917c9a397f9ba61cf66773843c703401ba753bd
Miloš Lalić <miloslalic9@gmail.com>
2018-01-24 21:16:02 +0100

    Initial commit

---
+++ components/Jumbotron.js
@@ -0,0 +1,16 @@
+const Jumbotron = () => (
+    <div className="jumbotron text-center">
+        <h1>Company</h1> 
+        <p>We specialize in blablabla</p> 
+        <form>
+            <div className="input-group">
+            <input type="email" className="form-control" size="50" placeholder="Email Address" required/>
+            <div className="input-group-btn">
+                <button type="button" className="btn btn-danger">Subscribe</button>
+            </div>
+            </div>
+        </form>
+    </div>
+);
+
+export default Jumbotron;
---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
+import About from './About';
+import Services from './Services';
+import Portfolio from './Portfolio';
+import Prices from './Prices';
+import Contact from './Contact';
+import Map from './Map';
+import Footer from './Footer';
+
+const Layout = () => (
+
+    <div>
+        <Navbar/>
+        <Jumbotron/>
+        <About/>
+        <Services/>
+        <Portfolio/>
+        <Prices/>
+        <Contact/>
+        <Map
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px`, width: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+        />
+        <Footer/>              
+    </div>
+);
+
+export default Layout;
